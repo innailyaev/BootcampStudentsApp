177826bf786037b9da9309f0c792cf9cfe71a5e2
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-03-12 02:58:58 +0200

    12/03/21#1

--- script/index.js
+++ script/index.js
@@ -1,30 +1,34 @@
 const table=document.querySelector('#table');
-const p=document.querySelector("#text");
+const select=document.querySelector("#select");
+const searchInput=document.querySelector("#searchInput");
 
 const usersApi='https://appleseed-wa.herokuapp.com/api/users/';
 const user='https://appleseed-wa.herokuapp.com/api/users';
 let usersData=[];
-let userDetails;
-// fetchUsersApi();
-fetchUser();
+let userDetails=[];
+let arrLength;
+// getUsersApi();
+createTable();
 
-async function fetchUsersApi(){
-    const callApi = await fetch(usersApi);
-    const data= await callApi.json();
-    usersData=data;
-    console.log(usersData);
-}
-
-async function fetchUser(){
-    const callApi = await fetch(`${user}/30`);
+async function getDetalisApi(id){
+    const callApi = await fetch(`${user}/${id}`);
     let data= await callApi.json();
-    userDetails=data;
-    // console.log(usersData);
+    return data;
 }
 
+async function getUsersApi(){
+    const response = await fetch(usersApi);
+    let usersResult= await response.json();
+    usersData = await Promise.all(
+        usersResult.map(async (u) => {
+            let detalis = await getDetalisApi(u.id);
+            return {id:u.id,firstName: u.firstName, lastName: u.lastName, capsule: u.capsule, 
+                city:detalis.city, age : detalis.age, gender:detalis.gender, hobby:detalis.hobby }})
+    )
+}
 
 async function createTable() {
-    await fetchUsersApi();
+    await getUsersApi();
     table.innerHTML += 
         `<thead>
             <tr>
@@ -38,25 +42,123 @@ async function createTable() {
             <th>Hobby</th>
         </tr>
       <thead>`
-      usersData.forEach((u) => {
-        console.log(u)
+        usersData.forEach( (u) => {
         table.innerHTML += 
-      `<tr>
+      `<tr id=${u.id}>
         <td>${u.id}</td>
         <td>${u.firstName}</td>
         <td>${u.lastName}</td>
         <td>${u.capsule}</td>
+        <td>${u.age}</td>
+        <td>${u.city}</td>
+        <td>${u.gender}</td>
+        <td>${u.hobby}</td>
+        <td><button id=${u.id} btn class=edit>Edit</button></td>
+        <td><button id=${u.id} btn class=delete>delete</button></td>
     </tr>`
     })
-    console.log("jjj",usersData);
+    console.log(usersData);
 }
 
-createTable();
+function removeUserFromArray(id){
+    usersData.forEach((u,index)=>{
+        if(u.id==id){
+            usersData.splice(index,1);
+        }
+    })
+    removeUserUi(id);   
+}
 
-// function removeUser(id){
-//     let users=usersData;
-//     users.splice(id,1);
-//     console.log("users", users);
-// }
+function removeUserUi(id){
+    let row=document.getElementById(`${id}`);
+    // row.style.display='none';    
+    row.parentNode.removeChild(row);
+}
+
+function searchByValue(){
+        let selectValue=searchByCategory();
+        let filter, tr, td, i, txtValue;
+        filter = searchInput.value.toUpperCase();
+        tr = table.getElementsByTagName("tr");
+      
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[selectValue];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function searchByCategory(){
+    switch(select.value){
+        case 'FN': return 1;
+        case 'LN': return 2;
+        case 'Cp': return 3;
+        case 'age': return 4;
+        case 'city': return 5;
+        case 'gender': return 6;
+        case 'hobby': return 7;
+    }
+}
+
+function upDate(id){
+    let newDetails=[];
+    let index=usersData.findIndex((p) => p.id ==id);
+
+    console.log(index);
+    let tr=document.getElementById(`${id}`);
+    console.log(tr);
+    tr.innerHTML=`<td >${id}</td>
+                <td><input id=FN class=editInput type=text value=${usersData[index].firstName}></td>
+                <td><input id=LN class=editInput type=text value=${usersData[index].lastName}></td>
+                <td><input id=CP class=editInput type=text value=${usersData[index].capsule}></td>
+                <td><input id=AG class=editInput type=text value=${usersData[index].age}></td>
+                <td><input id=CT class=editInput type=text value=${usersData[index].city}></td>
+                <td><input id=GE class=editInput type=text value=${usersData[index].gender}></td>
+                <td><input id=HB class=editInput type=text value=${usersData[index].hobby}></td>
+                <td><button id=${id} class=confirm>Confirm</button></td>
+                <td><button id=${id} class=cancel>Cancel</button></td>`
+    
+        for(let i=1;i<tr.cells.length;i++){
+            console.log(tr.children[i].firstChild.value);
+
+        }
+        console.log(tr.cells.length);
+        // console.log(tr.children[i].firstChild.value
 
 
+}
+
+function confirm(){
+
+
+}
+
+//Event listener
+table.addEventListener('click',(e)=>{
+    
+    
+    console.log(e.target.getAttribute('class'));
+    let row=e.target.getAttribute('id');
+    console.log(row);
+    switch(e.target.getAttribute('class')){
+        case('delete'):
+            removeUserFromArray(row);
+            break;
+        case ('edit'):
+            upDate(row);
+            break;
+        case ('confirm'):
+            confirm(row);
+            break;    
+    }
+    
+})
+
+searchInput.addEventListener('keyup',searchByValue);
